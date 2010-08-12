440c9e9d297b22f31ffbf0066db8fc5ff8a0c72b
Manu Sporny <msporny@digitalbazaar.com>
2010-08-12 17:43:53 -0400

    Updated PaySwarm OAuth demo client-side code.

--- demos/oauth/payswarm.php
+++ demos/oauth/payswarm.php
@@ -38,7 +38,12 @@ try
    // check the state of the payment token
    if($ptok['state'] == 0)
    {
-      print_r("<pre>STATE: 0</pre>");
+      $fh = fopen("/tmp/ps-oauth-state-0.txt", "w");
+      fwrite($fh, "STATE: 0\n");
+      fwrite($fh, "GPARAMS: " . print_r($_GET, true) . "\n");
+      fwrite($fh, "PPARAMS: " . print_r($_POST, true) . "\n");
+      fwrite($fh, "PTOK: " . print_r($ptok, true) . "\n");
+      fclose($fh);
       // State 0 - Generate request token and redirect user to payswarm site 
       // to authorize
       $request_token_info = $oauth->getRequestToken(
@@ -51,20 +56,27 @@ try
 
       // Save the token and the secret, which will be used later
       $ps->save($tok);
-      //header('Location: https://localhost:19100' .
-      //   '/api/3.2/oauth1/tokens/authorized?oauth_token='.$tok['token']);
+      header('Location: https://localhost:19100' .
+         '/content/authorize.html?oauth_token='.$tok['token']);
       exit;
    }
    else if($ptok['state'] == 1)
    {
-      print_r("<pre>STATE: 1</pre>");
+      $fh = fopen("/tmp/ps-oauth-state-1.txt", "w");
+      fwrite($fh, "STATE: 1\n");
+      fwrite($fh, "GPARAMS: " . print_r($_GET, true) . "\n");
+      fwrite($fh, "PPARAMS: " . print_r($_POST, true) . "\n");
+      fwrite($fh, "PTOK: " . print_r($ptok, true) . "\n");
+      fclose($fh);
       // State 1 - Handle callback from payswarm and get and store an access token
       $oauth->setToken($_GET['oauth_token'], $ptok['secret']);
       $access_token_info = $oauth->getAccessToken(
          'https://localhost:19100/api/3.2/oauth1/tokens');
+      $tok['id'] = $ptok['id'];
       $tok['state'] = 2;
       $tok['token'] = $access_token_info['oauth_token'];
       $tok['secret'] = $access_token_info['oauth_token_secret'];
+      $tok['amount'] = '0.2'; // FIXME: Make this read the actual amount value
       $ps->save($tok); // Save the access token and secret
       $ptok['token'] = $tok['token'];
       $ptok['secret'] = $tok['secret'];
@@ -73,18 +85,25 @@ try
    }
    else if($ptok['state'] == 2)
    {
+      $fh = fopen("/tmp/ps-oauth-state-2.txt", "w");
+      fwrite($fh, "STATE: 1\n");
+      fwrite($fh, "GPARAMS: " . print_r($_GET, true) . "\n");
+      fwrite($fh, "PPARAMS: " . print_r($_POST, true) . "\n");
+      fwrite($fh, "PTOK: " . print_r($ptok, true) . "\n");
+      fclose($fh);
      // State 2 - Authorized. We can just use the stored access token
      $oauth->setToken($ptok['token'], $ptok['secret']);
      $params = array(
         'asset' => 'http://example.org/news/myarticle.html',
-        'license' => 'http://example.org/licenses/personal-use');
+        'license' => 'http://example.org/licenses/personal-use',
+        'license_hash' => '866f3f9540e572e8cc4467f470a869242db201ba');
      $oauth->fetch('https://localhost:19100/api/3.2/oauth1/contracts', $params);
      $json = json_decode($oauth->getLastResponse());
      /*
      $debug = $oauth->getLastResponseInfo();
      print_r($debug);
      */
-     print_r('GOT: <pre>' . $json . '</pre>');
+     print_r('GOT: <pre>' . print_r($json, true) . '</pre>');
    }
 }
 catch(OAuthException $E)

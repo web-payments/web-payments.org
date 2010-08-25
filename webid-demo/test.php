b66d73c8c4d186379e3060740a4d6b32215ce72b
Dave Longley <dlongley@digitalbazaar.com>
2010-08-25 01:13:55 -0400

    Added public key comparison code.

--- webid-demo/test.php
+++ webid-demo/test.php
@@ -5,6 +5,9 @@ require_once('check_public_key.inc');
 $info = get_certificate_info();
 print_r($info);
 
+$rv = compare_public_keys($info['publicKey'], $info['publicKey']);
+echo 'result: "' . $rv . '"';
+
 //get_webid_rdf($info['webID']);
 
 ?>

e9deddbcde90827029ae07b262d68c26eb53ad64
Manu Sporny <msporny@digitalbazaar.com>
2014-11-29 15:45:15 -0500

    Add support for listing Final CG specs in spec index.

--- specs/index.php
+++ specs/index.php
@@ -6,7 +6,6 @@ function dirContents($dir)
    $contents = scandir($dir);
    rsort($contents);
 
-   print("Previous Drafts: ");
    $items = array();
    foreach($contents as $item)
    {
@@ -16,6 +15,11 @@ function dirContents($dir)
       }
    }
    print(implode($items, ", "));
+   
+   if(count($items) === 0) 
+   {
+      print "None.";
+   }
 }
 ?>
 <!-- ==== HEADER -->
@@ -44,7 +48,7 @@ innovate is a fundamental part of what we do.
 An outline of the design principles and general philosophy used to guide the 
 standards making process behind the technology that the Web Payments group 
 creates. The principles are meant to be a set of general guidelines rather than 
-a set of prescriptive rules. <?php dirContents("ED/design-principles"); ?>
+a set of prescriptive rules. Time-stamped drafts: <?php dirContents("ED/design-principles"); ?>
       </p>
       <p>
 <a href="source/use-cases">Use Cases</a>:
@@ -54,14 +58,15 @@ royalty-free specifications that allow people on the Web to send each other
 money as easily as they exchange instant messages and e-mail today. The 
 following use cases focus on concrete scenarios that the technology created
 by the group should enable.
-<?php dirContents("ED/use-cases"); ?>
+Time-stamped drafts: 
+<?php dirContents("FCGS/use-cases"); ?>, <?php dirContents("ED/use-cases"); ?>
       </p>
       <p>
 <a href="source/roadmap">Roadmap</a>:
 The Web Payments CG Roadmap outlines the proposed technology stack and 
 development timeline for the set of technologies being worked on by the 
 Web Payments Community Group.
-<?php dirContents("ED/roadmap"); ?>
+Time-stamped drafts: <?php dirContents("ED/roadmap"); ?>
       </p>
     </div><!-- col-lg-6 -->
   </div><!-- row -->
@@ -81,11 +86,11 @@ Web Payments Community Group.
 commerce portion of the PaySwarm architecture; enabling the decentralized 
 listing of assets for sale and the transaction of those assets resulting in a 
 digitally verifiable receipt between the buyer and the vendor.
-<?php dirContents("ED/web-commerce"); ?>
+Time-stamped drafts: <?php dirContents("ED/web-commerce"); ?>
       </p>
       <p>
 <a href="source/web-price-indexes">Pricing Indices</a>: Vendors can select one of the available online price indexing services, which could be a currency exchange rate, or another type of index more relevant to the scope and dynamics of their particular business that would enable greater price stability, variability with key input prices, or some other criterion.
-<?php dirContents("ED/web-price-indexes"); ?>
+Time-stamped drafts: <?php dirContents("ED/web-price-indexes"); ?>
       </p>
     </div><!-- col-lg-6 -->
   </div><!-- row -->
@@ -104,7 +109,7 @@ digitally verifiable receipt between the buyer and the vendor.
 <a href="source/web-payments">Web Payments</a>: The base layer of the
 Web Payments architecture; enables the creation of a monetary transaction 
 between two participants on the Web.     
-<?php dirContents("ED/web-payments"); ?>
+Time-stamped drafts: <?php dirContents("ED/web-payments"); ?>
       </p>
       <p>
 <a href="source/web-commerce-api">Web Commerce API</a>: Outlines a browser 
@@ -112,25 +117,25 @@ polyfill that makes financial transactions easier to initiate and verify
 while also making them more secure. The solution is designed to work with both 
 proprietary (PayPal, Google Wallet) and non-proprietary (PaySwarm, Bitcoin, 
 Ripple) payment solutions.
-<?php dirContents("ED/web-commerce-api"); ?>
+Time-stamped drafts: <?php dirContents("ED/web-commerce-api"); ?>
       </p>
 
       <p>
 <a href="source/payment-intents">Payment Intents</a>: The parameterized 
 transactions layer of the Web Payments architecture; enables decentralized, 
 open crowd-funding over the Web.
-<?php dirContents("ED/payment-intents"); ?>
+Time-stamped drafts: <?php dirContents("ED/payment-intents"); ?>
       </p>
       <p>
 <a href="source/vocabs/commerce.html">Commerce Vocabulary</a>: The Web vocabulary 
 that is used to describe commercial transactions.
-<?php dirContents("ED/vocabs/commerce"); ?>
+Time-stamped drafts: <?php dirContents("ED/vocabs/commerce"); ?>
       </p>
       <p>
 <a href="source/vocabs/payswarm.html">PaySwarm Vocabulary</a>: The Web vocabulary 
 that is used to describe specific concepts and properties on a 
 Web Payments network.
-<?php dirContents("ED/vocabs/payswarm"); ?>
+Time-stamped drafts: <?php dirContents("ED/vocabs/payswarm"); ?>
       </p>
       <p>
 <a href="source/vocabs/creditcard.html">Credit Card Vocabulary</a>: The Web vocabulary 
@@ -154,7 +159,7 @@ that is used to describe credit cards.
 of the Web Payments architecture; enabling the decentralized listing of assets 
 for sale and the transaction of those assets resulting in a digitally 
 verifiable receipt between the buyer and the vendor. 
-<?php dirContents("ED/web-commerce"); ?>
+Time-stamped drafts: <?php dirContents("ED/web-commerce"); ?>
       </p>
     </div><!-- col-lg-6 -->
   </div><!-- row -->
@@ -173,7 +178,7 @@ verifiable receipt between the buyer and the vendor.
 <a href="source/identity-credentials/">Identity Credentials</a>: A decentralized 
 identity mechanism for the Web that allows arbitrary Linked Data to be read from 
 and written to an identity URL.
-<?php dirContents("ED/identity-credentials"); ?>
+Time-stamped drafts: <?php dirContents("ED/identity-credentials"); ?>
       </p>
     </div><!-- col-lg-6 -->
   </div><!-- row -->
@@ -192,36 +197,36 @@ and written to an identity URL.
 <a href="source/http-signatures">HTTP Signatures</a>: A digital signature
 mechanism for the HTTP protocol that adds origin authentication and message 
 integrity to HTTP requests.
-<?php dirContents("ED/http-signatures"); ?>
+Time-stamped drafts: <?php dirContents("ED/http-signatures"); ?>
       </p>
       <p>
 <a href="source/http-signature-nonces">HTTP Signature Nonces</a>: An extension
 to the HTTP Signatures specification that enables replay protection when 
 messages are sent over a non-secured HTTP connection.
-<?php dirContents("ED/http-signature-nonces"); ?>
+Time-stamped drafts: <?php dirContents("ED/http-signature-nonces"); ?>
       </p>
       <p>
 <a href="source/http-signature-trailers">HTTP Signature Trailers</a>: An
 extension to the HTTP Signatures specification that enables digital signatures
 to be applied to content that is streamed, such as audio and video, via 
 HTTP Trailers.
-<?php dirContents("ED/http-signature-trailers"); ?>
+Time-stamped drafts: <?php dirContents("ED/http-signature-trailers"); ?>
       </p>
       <p>
 <a href="source/secure-messaging">Secure Messaging</a>: A secure and verifiable 
 messaging mechanism built using Linked Data principles to produce a distributed
 Public Key Infrastructure for the Web.
-<?php dirContents("ED/secure-messaging"); ?>
+Time-stamped drafts: <?php dirContents("ED/secure-messaging"); ?>
       </p>
       <p>
 <a href="source/vocabs/security.html">Security Vocabulary</a>: the Web vocabulary 
 that is used to describe mechanisms for expressing digital signatures and 
 encrypting and decrypting messages.
-<?php dirContents("ED/vocabs/security"); ?>
+Time-stamped drafts: <?php dirContents("ED/vocabs/security"); ?>
       </p>
 <a href="source/http-signatures-audit">Security Considerations for HTTP 
 Signatures</a>: A complete security audit of the HTTP Signatures specification.
-<?php dirContents("ED/http-signatures-audit"); ?>
+Time-stamped drafts: <?php dirContents("ED/http-signatures-audit"); ?>
       </p>
     </div><!-- col-lg-6 -->
   </div><!-- row -->
@@ -239,7 +244,7 @@ Signatures</a>: A complete security audit of the HTTP Signatures specification.
       <p>
 <a href="source/web-api">PaySwarm Web API</a>: The initial PaySwarm protocol 
 designed to allow open listing, buying, and selling digital assets over the Web.
-<?php dirContents("ED/web-api"); ?>
+Time-stamped drafts: <?php dirContents("ED/web-api"); ?>
       </p>
     </div><!-- col-lg-6 -->
   </div><!-- row -->
